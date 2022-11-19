31dffd46566e6eb44ba7d0e9d20e217492703d3b
Joanna <astronautnemo@gmail.com>
2022-11-19 12:20:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+// No lnger has DOM node #main
+const main = document.getElementById("main");
+main.remove();
+
+// Has a 'newHeader' variable pointing to a <h1> node
+const newHeader = document.createElement('h1');
+
+// 'newHeader' node has an id of victory
+newHeader.id = 'victory';
+
+// 'newHeader' has the text "YOUR-NAME" is the champion" in it
+newHeader.textContent = 'Jo is the champion';
